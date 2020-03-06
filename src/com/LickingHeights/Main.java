5a347f9c7bc429f1285bddd219bb2858645356a6
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-03-06 12:20:23 -0500

    UserDataCaptured

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,29 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ModulusClock();
+    }
+    public static void ModulusClock(){
+
+        Scanner userInput = new Scanner(System.in);
+    System.out.println("What is the hour?");
+
+    int hour = userInput.nextInt();
+
+    System.out.println("What are the minutes?");
+
+    int minutes = userInput.nextInt();
+
+
+
+
+
+    }
+
+
+
+}
